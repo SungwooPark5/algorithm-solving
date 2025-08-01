64533a73621c57bbffa3be6967cf586bfdc423b1
swpark <swpark.biz@gmail.com>
2025-08-01 14:10:23 +0900

    Comment 08해시-23베스트 앨범

--- programmers/08-hash/23.베스트앨범.py
+++ programmers/08-hash/23.베스트앨범.py
@@ -12,9 +12,12 @@ def solution(genres, plays):
         genres_dict[genre].append((i, play))
         play_dict[genre] += play
 
+    # dict.items()는 key, value를 tuple 로 변환
     sorted_genres = sorted(play_dict.items(), key=lambda x: x[1], reverse=True)
 
     for genre, _ in sorted_genres:
+        # tuple의 정렬은 기본적으로 모든 값을 고려해줌
+        # play에 음수를 취하고 오름차순 정렬함. 두 번째도 오름차순(id가 작은 게 먼저)가 됨
         sorted_songs = sorted(genres_dict[genre], key=lambda x: (-x[1], x[0]))
         answer.extend([idx for idx, _ in sorted_songs[:2]])
 
