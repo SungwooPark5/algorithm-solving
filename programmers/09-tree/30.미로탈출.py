22c868f427e201dce2c8eacfeef493f415a1a890
swpark <swpark.biz@gmail.com>
2025-08-20 13:43:18 +0900

    Solve 09트리-30.미로 탈출
    책을 읽고, 직접 풀이를 작성해봄. visited에서 왜 두 개의 데이터를 저장하는지 고민해보기.

--- programmers/09-tree/30.미로탈출.py
+++ programmers/09-tree/30.미로탈출.py
@@ -1,7 +1,73 @@
-# 아래의 내용은 책에 있는 풀이를 적은 것으로
-# 실제로는 시간 안에 풀지 못했음
-# 시간 들여서 다음에 다시 풀어볼 것
-# 문제: https://school.programmers.co.kr/learn/courses/30/lessons/159993
+"""
+책을 보고 직접 다시 푼 코드
+문제: https://school.programmers.co.kr/learn/courses/30/lessons/159993
+"""
+
+from collections import deque
+
+
+def find_position(maps, char):
+    for y, _ in enumerate(maps):
+        for x, _ in enumerate(maps[0]):
+            if maps[y][x] == char:
+                return x, y
+    return None
+
+
+def is_valid_move(maps, x, y, MAX_COL, MAX_ROW):
+    return -1 < x < MAX_ROW and -1 < y < MAX_COL and maps[y][x] != "X"
+
+
+def solution(maps):
+    q = deque()  # BFS를 위한 큐
+
+    MAX_COL, MAX_ROW = len(maps), len(maps[0])
+    # visited[y][x][0]은 lever를 당기지 않고 도달한 경우
+    # visited[y][x][1]은 lever를 당기고 도달한 경우
+    visited = [
+        [[False for _ in range(2)] for _ in range(MAX_ROW)] for _ in range(MAX_COL)
+    ]
+
+    nx = [-1, 1, 0, 0]
+    ny = [0, 0, -1, 1]
+
+    sx, sy = find_position(maps, "S")
+
+    visited[sy][sx][0] = True
+    q.append((sx, sy, 0, 0))  # position, time, lever 여부
+
+    while q:
+        x, y, time, k = q.popleft()
+
+        if maps[y][x] == "E" and k == 1:
+            return time
+
+        for i in range(4):
+            next_x, next_y = x + nx[i], y + ny[i]
+
+            if not is_valid_move(maps, next_x, next_y, MAX_COL, MAX_ROW):
+                continue
+
+            # 현재 위치가 L인 경우, 다시 돌아갈 수도 있기 때문에
+            # k의 값을 1로 변경하여 visited가 겹치지 않도록 함
+            if maps[y][x] == "L":
+                k = 1
+
+            # 방문하지 않은 경우, q에 넣어 다음 라운드에 방문할 수 있도록 함
+            # 결과적으로, 가까운 경로부터 방문하여 너비 우선 탐색이 구현됨
+            if not visited[next_y][next_x][k]:
+                visited[next_y][next_x][k] = True
+                q.append((next_x, next_y, time + 1, k))
+
+    return -1
+
+
+"""
+아래의 내용은 책에 있는 풀이를 적은 것으로
+처음 시도에서 시간 안에 풀지 못했음
+"""
+
+"""
 from collections import deque
 
 
@@ -50,3 +116,4 @@ def solution(maps):
                 append_to_queue(ny, nx, k, time, visited, q)
 
     return -1
+"""
